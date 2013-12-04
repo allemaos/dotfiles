dcfb6d0d4737ef88ee1dab938424b41bfc2ca0c0
Giorgos Andreou <allemaos@gmail.com>
2013-12-04 23:10:43 +0200

    changed to backup all dotted files

--- bin/dotfiles_backup.sh
+++ bin/dotfiles_backup.sh
@@ -17,13 +17,13 @@ function backup() {
 
 ls -al | grep -v "\.$" | grep -v "backup" | grep -v "bin"  | awk '{print "backup:"$9} {backup "~/"$9 $tmp}'
 
-backup ~/.bashrc $tmp
-backup ~/.bash_profile $tmp
-backup ~/.gitconfig $tmp
-backup ~/.gitignore_global $tmp
-backup ~/.vimrc $tmp
-backup ~/.vim/ $tmp
-backup ~/bin/ $tmp
+#backup ~/.bashrc $tmp
+#backup ~/.bash_profile $tmp
+#backup ~/.gitconfig $tmp
+#backup ~/.gitignore_global $tmp
+#backup ~/.vimrc $tmp
+#backup ~/.vim/ $tmp
+#backup ~/bin/ $tmp
 
 
 NOW=$(date +"%Y%m%d%H%M")
