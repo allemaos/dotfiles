83822f6fb77a770c670afbcafff5dc944861e3e7
Giorgos Andreou <allemaos@gmail.com>
2014-01-10 14:26:48 +0200

    added copy script for partial copying files and directories

---
+++ bin/copy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This is a filtered copy script for files and directories 
+# arguments
+# $1 is the source directory
+# $2 is the destination directory 
+# $3 is the echoed name of the running script
+
+src=$1
+des=$2
+prompt=$3
+
+mkdir $tmp
+
+function my_copy() {
+	item=$1
+	tmp=$2
+	if [ -f $item -o -d $item ]; then
+		if [ ! -d $tmp ]; then mkdir $tmp; fi
+		cp -r $item $tmp
+	else
+		echo "$item doesn't exist"
+	fi
+}
+
+#backup sequetially all dotted files without .git backup and bin dir's
+ls -al --ignore=".git" | tail -n +2 | grep -v "\.$" | grep -v "backup" | grep -v "bin"  | awk '{print "$prompt:"$9} {my_copy "$src/"$9 $des}'
+
