e30878916937ec17b0d7de94dade91a9c2d1aa91
Giorgos Andreou <allemaos@gmail.com>
2014-10-01 15:53:52 +0300

    changes to backup apps and .vim directories

--- bin/dotfiles_backup.sh
+++ bin/dotfiles_backup.sh
@@ -8,9 +8,14 @@ mkdir $tmp
 #ls -al --ignore=".git" | tail -n +2 | grep -v "\.$" | grep -v "backup" | grep -v "bin"  | awk '{print "backup:"$9} {backup "~/"$9 $tmp}'
 ls -al --ignore=".git" | tail -n +2 | grep -v "\.$" | grep -v "backup" | grep -v "bin"  | awk '{print $NF}' | xargs -I{} cp ~/{} $tmp
 
+dir=apps/	
+mkdir $tmp/$dir; ls -al $dir | tail -n +2 | grep -v "\.$" | awk '{print $NF}' | xargs -I{} cp -r ~/apps/{} $tmp/$dir
+dir=.vim/	
+mkdir $tmp/$dir; ls -al $dir | tail -n +2 | grep -v "\.$" | awk '{print $NF}' | xargs -I{} cp -r ~/apps/{} $tmp/$dir
+
 back="./backup"
 if [ ! -d "$back" ]; then mkdir "$back"; fi
-tar -cf "$back/dotfiles-$NOW.tar.gz" $tmp
+tar -zcf "$back/dotfiles-$NOW.tar.gz" $tmp
 #rm -rf $tmp
 
 
